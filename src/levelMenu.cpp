28850e56887ae921470384f565b38f8e77646899
Vulpes19 <baioumyaymann@gmail.com>
2023-03-15 15:41:55 +0100

    fixed problem with cursor in level menu

--- src/levelMenu.cpp
+++ src/levelMenu.cpp
@@ -23,8 +23,9 @@
 #define NOT_ON_BUTTON_TWO  !(cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 - 80 && cursor.getY() <= HEIGHT / 2 - 80 + 70)
 #define ON_BUTTON_THREE cursor.getX() >= 20 && cursor.getX() <= 160 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 + 20 + 70
 #define NOT_ON_BUTTON_THREE !(cursor.getX() >= 20 && cursor.getX() <= 160 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 + 20 + 70)
-#define ON_BUTTON_FOUR cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 - 20 + 70 
-#define NOT_ON_BUTTON_FOUR !(cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 - 20 + 70)
+#define ON_BUTTON_FOUR cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 + 20 + 70
+// #define NOT_ON_BUTTON_FOUR !(cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= cursor.getY() <= HEIGHT / 2 + 20 + 70)
+#define NOT_ON_BUTTON_FOUR !(cursor.getX() >= WIDTH - 160 && cursor.getX() <= WIDTH - 20 && cursor.getY() >= HEIGHT / 2 + 20 && cursor.getY() <= HEIGHT / 2 + 20 + 70)
 
 const   std::string LevelMenu::state = "Level";
 
@@ -41,7 +42,10 @@ int	LevelMenu::update( void )
 {
     Vector cursor = InputHandler::getInstance().getCursorPosition();
 	if ( InputHandler::getInstance().getMouseButton( LEFT ) )
-    {
+    { 
+        if ( cursor.getX() >= WIDTH - 50 && cursor.getX() <= WIDTH - 10 &&
+                cursor.getY() >= HEIGHT - 50 && cursor.getY() <= HEIGHT - 10 )
+            return (QUIT_BUTTON);
         if ( ON_BUTTON_ONE )
         {
             currentMode = "standard";
